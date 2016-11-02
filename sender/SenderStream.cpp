702fc3fa522625f800b5fc12d676dc3928f4f978
mijara <marjara35@gmail.com>
2016-11-02 00:29:47 -0300

    Attach pool

--- sender/SenderStream.cpp
+++ sender/SenderStream.cpp
@@ -10,22 +10,24 @@ SenderStream::SenderStream(BufferPool & pool, int linger) :
 SenderStream::~SenderStream()
 {
     // delete all allocated flows.
-    typedef std::map<std::string, ProtobufSender*>::iterator it_type;
+    typedef std::map<std::string, SenderFlow*>::iterator it_type;
     for(it_type iterator = flows.begin(); iterator != flows.end(); iterator++) {
         delete(flows[iterator->first]);
     }
 }
 
-ProtobufSender * SenderStream::create_flow(std::string flow_name, std::string connect)
+template <class T>
+T * SenderStream::create_flow(std::string flow_name, std::string connect)
 {
-    ProtobufSender * flow = new ProtobufSender(connect, pool, linger);
+    T * flow = new T(connect, linger);
+    flow->attach_buffer_pool(pool);
 
     flows[flow_name] = flow;
 
     return flow;
 }
 
-ProtobufSender * SenderStream::get_flow(std::string name)
+SenderFlow * SenderStream::get_flow(std::string name)
 {
     if (exist_flow(name)) {
         return flows[name];
