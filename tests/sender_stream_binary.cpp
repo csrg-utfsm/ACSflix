dc3d580d74568ada5f068a5380ac9bf7a393ef8d
Leonardo Pizarro <lepizarr@inf.utfsm.cl>
2017-01-18 16:05:51 -0300

    Fixed place. ./sender's main is in tests/sender_stream_binary.cpp rather than tests/sender.cpp

--- tests/sender_stream_binary.cpp
+++ tests/sender_stream_binary.cpp
@@ -20,6 +20,7 @@ int main(int argc, char * argv[])
 
     clock_t begin = clock();
     size_t block_count = 0;
+    size_t accumulated_sent_size;
 
     std::cout << "Beginning transmission..." << std::endl;
     uint32_t i = 0;
@@ -32,6 +33,7 @@ int main(int argc, char * argv[])
         if (block_count % 256 == 0) {
 	    std::cout << "Sent: " << block_count << " blocks of " << sent_size << " bytes each." << std::endl;
         }
+	std::cout << "Accumulated: " << accumulated_sent_size << std::endl;
     }
 
     clock_t end = clock();
