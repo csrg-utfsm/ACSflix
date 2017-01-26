aa603ba00b9a98365d464224cad9b91a6d1dab3a
mijara <marjara35@gmail.com>
2017-01-26 11:16:56 -0300

    Add Flow headers.

---
+++ SenderFlow.hpp
@@ -0,0 +1,19 @@
+#include <string>
+
+// SenderFlow implements the sender behavior in the ACSFlix model, using the 
+// PUSH/PULL pattern. It creates it's own context and PUSH Socket, workloads
+// are sent with the send() method.
+class SenderFlow {
+private:
+    // ZMQ Context.
+    void * m_context;
+
+    // ZMQ Socket.
+    void * m_socket;
+
+public:
+    // SenderFlow creates a new SenderFlow bound to the bind parameter
+    // given. The bind parameter must be in the zmq_bind format
+    // (see http://api.zeromq.org/4-2:zmq-bind).
+    SenderFlow(std::string bind);
+}
--- WorkerFlow.hpp
+++ WorkerFlow.hpp
@@ -1,5 +1,28 @@
+#include <string>
+
+// WorkerFlow implements the behavior of a Worker in the ACSFlix model, using
+// the PUSH/PULL pattern. It creates it's own context and PULL socket, and work
+// is done through the work() method.
 class WorkerFlow {
 private:
-    void * m_context;
+    // ZeroMQ context.
+    void * m_context;   
+
+    // ZeroMQ socket.
+    void * m_socket;    
+
+    // WorkerBuffer to store incoming data, it will be reused every time
+    // work() is called. It is allocated with a fixed size.
+    char m_buffer[524288];
+
 public:
+    // WorkerFlow creates a new Worker Flow, connected to a sender using
+    // the connect parameter. The connect parameter is formatted as ZMQ
+    // zmq_connect format (see http://api.zeromq.org/4-2:zmq-connect).
+    WorkerFlow(std::string connect);
+
+    // Work tells this worker to receive a workload and process it.
+    // Returns true if there's more work to do. A worker should stop
+    // working when it returns false.
+    bool work();
 }
