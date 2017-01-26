9f6934ef93b4975688c9e4e7a70898d57a7dfc61
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2017-01-26 17:08:56 -0300

    Add debug option

--- cmd/sender.cpp
+++ cmd/sender.cpp
@@ -1,13 +1,19 @@
 #include "SenderFlow.h"
 
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <cassert>
 #include <cstdio>
+#include <unistd.h>
+
+void __raii_file(FILE **file)
+{
+    if (file) fclose(*file);
+}
 
 int main(int argc, char * argv[]) {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " bind file" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " bind file|debug" << std::endl;
         return 0;
     }
 
@@ -18,15 +24,27 @@ int main(int argc, char * argv[]) {
     // create the Callback and WorkerFlow with the connect argument.
     SenderFlow sf(bind);
 
-    FILE * file = fopen(argv[2], "r");
-    assert(file);
+    if (file_path == "debug") {
+        int msg_counter = 1;
+        char message[256];
 
-    char buffer[524288];
+        while (msg_counter < 11) {
+            sprintf(message, "Message #%d\n", msg_counter++);
+            sf.send(message, strlen(message));
+            std::cout << "Sent: " << message;
+            sleep(1);
+        }
+    } else {
+        FILE * file __attribute__((cleanup (__raii_file))) = NULL;
+        file = fopen(argv[2], "r");
+        assert(file);
 
-    while (!feof(file)) {
-        size_t read = fread(buffer, 1, sizeof(buffer), file);
-        sf.send(buffer, read);
+        char buffer[524288];
+
+        while (!feof(file)) {
+            size_t read = fread(buffer, 1, sizeof(buffer), file);
+            sf.send(buffer, read);
+        }
     }
 
-    fclose(file);
 }
